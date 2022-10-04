15122f90d24f9171b8040b18e05f4f7468533c19
CalvinDjaja <calvindjaja6@.com>
2022-10-04 18:05:46 +0700

    Add project files.

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TugasBootcampNET.Models;
+
+namespace TugasBootcampNET.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}   
