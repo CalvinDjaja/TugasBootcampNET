274f639fba4510283a2e2660d33ea21e67997437
CalvinDjaja <calvindjaja6@.com>
2022-10-07 20:02:24 +0700

    Menambahkan authenticate method

--- DAL/UserEF.cs
+++ DAL/UserEF.cs
@@ -1,20 +1,51 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using TugasBootcampNET.DTO;
+using TugasBootcampNET.Helpers;
 
 namespace TugasBootcampNET.DAL
 {
     public class UserEF : IUser
     {
         private readonly UserManager<IdentityUser> _userManager;
-        public UserEF(UserManager<IdentityUser> userManager)
+        private readonly AppSettings _appSettings;
+        public UserEF(UserManager<IdentityUser> userManager,
+            IOptions<AppSettings> appSettings)
         {
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
-        public Task<UserGetDTO> Authenticate(AddUserDTO user)
+        public async Task<UserGetDTO> Authenticate(AddUserDTO user)
         {
-            throw new NotImplementedException();
+            var currUser = await _userManager.FindByNameAsync(user.Username);
+            var userResult = await _userManager.CheckPasswordAsync(currUser, user.Password);
+            if (!userResult)
+                throw new Exception($"Authentication failed");
+
+            UserGetDTO userWithToken = new UserGetDTO
+            {
+                Username = user.Username
+            };
+
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, user.Username));
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            userWithToken.Token = tokenHandler.WriteToken(token);
+            return userWithToken;
         }
 
         public Task<IEnumerable<UserGetDTO>> GetAll()
